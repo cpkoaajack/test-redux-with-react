17ad6910b0864885675a988aae1841fc8c2ae8da
cpkoaajack <cpkoaa@ust.hk>
2020-05-26 22:02:59 +0800

    Add cake store using Hooks with redux

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 //Components
 import CakeContainer from './components/CakeContainer';
+import UseHooksCakeContainer from './components/UseHooksCakeContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className='App'>
         <CakeContainer />
+        <UseHooksCakeContainer />
       </div>
     </Provider>
   );
---
+++ src/components/UseHooksCakeContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { buyCake } from '../redux';
+
+const UseHooksCakeContainer = () => {
+  const numOfCake = useSelector((state) => state.numOfCake);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>Number of cake(s) using Hooks {numOfCake}</h1>
+      <button onClick={() => dispatch(buyCake())}>Buy cake using Hooks</button>
+    </div>
+  );
+};
+
+export default UseHooksCakeContainer;
